80e143d2470e1f96a892712321bdf544cec3f63a
Liquid-system <80927666+Liquid-system@users.noreply.github.com>
2023-01-26 15:23:34 +0900

    fix: dial.nvim

--- lua/plugins/dial.lua
+++ lua/plugins/dial.lua
@@ -51,10 +51,10 @@ return {
       require("dial.map").dec_normal()
     end, { noremap = true })
     vim.keymap.set("v", "<C-a>", function()
-      require("dial.map").inc_visual "visual"
+      require("dial.map").inc_visual()
     end, { noremap = true })
     vim.keymap.set("v", "<C-x>", function()
-      require("dial.map").dec_visual "visual"
+      require("dial.map").dec_visual()
     end, { noremap = true })
     vim.keymap.set("v", "g<C-a>", function()
       require("dial.map").inc_gvisual()
